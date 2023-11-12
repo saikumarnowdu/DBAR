75dc91b27884f17ba150e33b785b371cbdf0aaaa
USER <USER@DESKTOP-UQERS01>
2023-11-13 01:41:38 +0530

    Add project files.

---
+++ DBAR/ViewModel/SettingVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBAR.Model;
+
+namespace DBAR.ViewModel
+{
+    class SettingVM : Utilities.ViewModelBase
+    {
+        private readonly PageModel _pageModel;
+        public bool Settings
+        {
+            get { return _pageModel.LocationStatus; }
+            set { _pageModel.LocationStatus = value; OnPropertyChanged(); }
+        }
+
+        public SettingVM()
+        {
+            _pageModel = new PageModel();
+            Settings = true;
+        }
+    }
+}
---
+++ DBAR/ViewModel/TransactionVM.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBAR.Model;
+
+namespace DBAR.ViewModel
+{
+    class TransactionVM : Utilities.ViewModelBase
+    {
+        private readonly PageModel _pageModel;
+        public decimal TransactionAmount
+        {
+            get { return _pageModel.TransactionValue; }
+            set { _pageModel.TransactionValue = value; OnPropertyChanged(); }
+        }
+
+        public TransactionVM()
+        {
+            _pageModel = new PageModel();
+            TransactionAmount = 5638;
+        }
+    }
+}
